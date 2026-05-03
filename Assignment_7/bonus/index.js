57bfead6e17bcd240b099f72d3e421f96ea2bf9f
markyouhanna12 <markyouhanna1@gmail.com>
2026-05-03 14:12:40 +0300

    Bonus part

---
+++ Assignment_7/bonus/index.js
@@ -0,0 +1,13 @@
+function removeElement(nums,val){
+    let k = 0
+    for (let i=0; i< nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i]
+            k++;
+        }
+    }
+    return k
+};
+
+const  result = removeElement([0,1,2,2,3,0,4,2],2)
+console.log(result);
